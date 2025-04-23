68e0e98edca24eaeddf47585008e92f027801386
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-23 13:11:55 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,23 +1,21 @@
-import { db } from "./firebase-config.js";
-import {
-  ref,
-  get
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import app from './firebase-config.js';
+import { getAuth, onAuthStateChanged, updateProfile, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
-const user = JSON.parse(localStorage.getItem("currentUser"));
-if (!user) window.location.href = "login.html";
+const auth = getAuth(app);
 
-const usernameEl = document.getElementById("username");
-const displayNameEl = document.getElementById("displayName");
-const userImage = document.getElementById("userImage");
-const lastSeen = document.getElementById("lastSeen");
+window.updateProfileName = function () {
+  const newName = document.getElementById("newDisplayName").value;
+  updateProfile(auth.currentUser, { displayName: newName }).then(() => {
+    alert("Display name updated!");
+  });
+};
 
-get(ref(db, "users/" + user.username)).then((snapshot) => {
-  if (snapshot.exists()) {
-    const data = snapshot.val();
-    usernameEl.textContent = user.username;
-    displayNameEl.textContent = data.displayName || "بدون اسم";
-    userImage.src = data.profileImage || "default.png";
-    lastSeen.textContent = data.lastSeen || "غير متاح";
-  }
+window.logout = function () {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
+};
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) window.location.href = "index.html";
 });
