6a25ad1dfd15c6ce0d49c890719fb0eb5e8a4fe9
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-23 16:42:38 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,27 @@
+// firebase.js
 
+// استيراد الحزمة
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+
+// إعدادات Firebase الخاصة بك
+const firebaseConfig = {
+  apiKey: "AIzaSyDkL37i0-pd885YbCBYOkADYQVQINcswhk",
+  authDomain: "messengerapp-58f7a.firebaseapp.com",
+  databaseURL: "https://messengerapp-58f7a-default-rtdb.firebaseio.com",
+  projectId: "messengerapp-58f7a",
+  storageBucket: "messengerapp-58f7a.appspot.com",
+  messagingSenderId: "46178168523",
+  appId: "1:46178168523:web:cba8a71de3d7cc5910f54e"
+};
+
+// تهيئة Firebase
+const app = initializeApp(firebaseConfig);
+
+// قاعدة البيانات
+const firebase = {
+  app,
+  database: getDatabase(app)
+};
+
+window.firebase = firebase;
