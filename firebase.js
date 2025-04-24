bdd48c7b97237383d21c04b9619dbb9c5f2a6f9e
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-24 14:31:18 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDkL37i0-pd885YbCBYOkADYQVQINcswhk",
   authDomain: "messengerapp-58f7a.firebaseapp.com",
-  databaseURL: "https://messengerapp-58f7a-default-rtdb.firebaseio.com",
   projectId: "messengerapp-58f7a",
   storageBucket: "messengerapp-58f7a.appspot.com",
   messagingSenderId: "46178168523",
-  appId: "1:46178168523:web:cba8a71de3d7cc5910f54e"
+  appId: "1:46178168523:web:cba8a71de3d7cc5910f54e",
+  databaseURL: "https://messengerapp-58f7a-default-rtdb.firebaseio.com"
 };
 
-// تشغيل Firebase
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
