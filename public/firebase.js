0be3e46a28c121ef4b638e81a70f3a3f18844360
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-24 20:06:55 +0300

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,14 @@
+// إعدادات Firebase الخاصة بك
+const firebaseConfig = {
+  apiKey: "AIzaSyDkL37i0-pd885YbCBYOkADYQVQINcswhk",
+  authDomain: "messengerapp-58f7a.firebaseapp.com",
+  databaseURL: "https://messengerapp-58f7a-default-rtdb.firebaseio.com",
+  projectId: "messengerapp-58f7a",
+  storageBucket: "messengerapp-58f7a.appspot.com",
+  messagingSenderId: "46178168523",
+  appId: "1:46178168523:web:cba8a71de3d7cc5910f54e"
+};
+
+// تشغيل Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
