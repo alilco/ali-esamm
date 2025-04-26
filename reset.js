4a3779fa5ccb1b2ea15d343fe6fa0c33c927e233
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-26 12:00:05 +0300

    Update and rename reset.js to reset-password.js

--- reset.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "./firebase.js";
-import { sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-document.getElementById("resetBtn").addEventListener("click", () => {
-  const email = document.getElementById("email").value.trim();
-  sendPasswordResetEmail(auth, email)
-    .then(() => alert("تم إرسال رابط إعادة تعيين كلمة المرور إلى بريدك."))
-    .catch(error => alert("فشل إرسال الرابط: " + error.message));
-});
