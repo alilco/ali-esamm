c0981ef631ef2d613d14778c00e8b235e10ff288
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-29 17:49:11 +0300

    Update and rename firebase.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkL37i0-pd885YbCBYOkADYQVQINcswhk",
+  authDomain: "messengerapp-58f7a.firebaseapp.com",
+  databaseURL: "https://messengerapp-58f7a-default-rtdb.firebaseio.com",
+  projectId: "messengerapp-58f7a",
+  storageBucket: "messengerapp-58f7a.firebasestorage.app",
+  messagingSenderId: "46178168523",
+  appId: "1:46178168523:web:cba8a71de3d7cc5910f54e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference to the database
+const database = firebase.database();
+const messagesRef = database.ref('messages');
+
+window.messagesRef = messagesRef; // Expose to script.js
