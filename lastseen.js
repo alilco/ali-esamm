385219684225be83884fc28688ed809e3facf315
F O [X] <123931406+alilco@users.noreply.github.com>
2025-04-29 16:57:18 +0300

    Delete lastseen.js

--- lastseen.js
+++
@@ -1,8 +0,0 @@
-const lastSeenRef = firebase.database().ref('users/' + user.uid + '/lastSeen');
-lastSeenRef.set(firebase.database.ServerValue.TIMESTAMP);
-
-// لعرض آخر ظهور في صفحة الملف الشخصي:
-firebase.database().ref('users/' + user.uid).once('value').then((snapshot) => {
-  const lastSeen = snapshot.val().lastSeen;
-  document.getElementById('last-seen').textContent = new Date(lastSeen).toLocaleString();
-});
